c1fe66f1cf528e7fceac7a01a2d1c5fbc116ffcf
ShaKend <shaunkennt@gmail.com>
2024-05-27 23:56:02 +0700

    tampilan mobile(blm klar)

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//toggle active
+const navmain = document.querySelector(".nav-main");
+const menu = document.getElementById("menu");
+
+menu.addEventListener("click", () => {
+  navmain.classList.toggle("active");
+});
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navmain.contains(e.target)) {
+    navmain.classList.remove("active");
+  }
+});
