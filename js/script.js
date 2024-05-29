6b541d498dce6da0e44d0c84ff5bcb60b638260a
ShaKend <shaunkennt@gmail.com>
2024-05-29 10:12:11 +0700

    event & news

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 //toggle active
 const navmain = document.querySelector(".nav-main");
 const menu = document.getElementById("menu");
+const popup = document.querySelector("#open-popup");
+const form = document.querySelector(".pop-up");
+const close = document.querySelector(".close");
+const overlay = document.querySelector(".overlay");
 
 menu.addEventListener("click", () => {
   navmain.classList.toggle("active");
@@ -9,5 +13,25 @@ menu.addEventListener("click", () => {
 document.addEventListener("click", function (e) {
   if (!menu.contains(e.target) && !navmain.contains(e.target)) {
     navmain.classList.remove("active");
+  }
+});
+popup.addEventListener("click", () => {
+  form.style.display = "block";
+  overlay.style.display = "block";
+});
+// document.getElementById("open-popup").addEventListener("click", function () {
+//   popup.style.display = "block";
+//   overlay.style.display = "block";
+//   //console.log("Button clicked!");
+// });
+// document.querySelector(".close").addEventListener("click", function () {
+//   popup.style.display = "none";
+//   overlay.style.display = "none";
+//   console.log("Button clicked!");
+// });
+document.addEventListener("click", function (e) {
+  if (overlay.contains(e.target) || close.contains(e.target)) {
+    form.style.display = "none";
+    overlay.style.display = "none";
   }
 });
